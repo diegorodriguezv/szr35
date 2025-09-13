f72cf7dc6ebceb3b573a28fca64acc0220ebf68a
diegorodriguezv <diegorodriguezv@gmail.com>
2025-09-13 12:20:34 -0500

    Moved some split configs to json.

--- config.h
+++ config.h
@@ -10,19 +10,9 @@
 #define DYNAMIC_KEYMAP_LAYER_COUNT 9
 #define DYNAMIC_KEYMAP_MACRO_COUNT 15
 
-// for responsive animation effects in both halves
-#define SPLIT_TRANSPORT_MIRROR
-
-#define SPLIT_USB_TIMEOUT_POLL 10
-#define SPLIT_WATCHDOG_TIMEOUT 3000
-
 #define WS2812_PWM_DRIVER PWMD3              // default: PWMD2
 #define WS2812_DMA_STREAM STM32_DMA1_STREAM2 // DMA Stream for TIMx_UP, see the respective reference manual for the appropriate values for your MCU.
 #define WS2812_DMA_CHANNEL 5                 // DMA Channel for TIMx_UP, see the respective reference manual for the appropriate values for your MCU.
 
 #define WEAR_LEVELING_LOGICAL_SIZE 2048
 #define WEAR_LEVELING_BACKING_SIZE (WEAR_LEVELING_LOGICAL_SIZE * 2)
-
-#define SPLIT_USB_DETECT
-#define SPLIT_WATCHDOG_ENABLE
-#define USB_POLLING_INTERVAL_MS 1
